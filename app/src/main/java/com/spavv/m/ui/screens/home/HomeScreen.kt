f9a2b24a5b9354817d3a9927f58d2f3138651b40
NguyenDat0809 <datngx.dev@gmail.com>
2025-02-08 11:02:23 +0700

    update: rename composable

--- app/src/main/java/com/spavv/m/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/spavv/m/ui/screens/home/HomeScreen.kt
@@ -1,7 +1,9 @@
 package com.spavv.m.ui.screens.home
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
@@ -19,6 +21,7 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
@@ -54,8 +57,8 @@ fun HomeScreen(modifier: Modifier = Modifier, navController: NavController, auth
                     top = 16.dp,
                     start = 16.dp,
                     end = 16.dp,
-                    bottom = 100.dp //height of navigation bar
-                    )
+                    bottom = 100.dp
+                )
                 .verticalScroll(scrollState),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -66,19 +69,107 @@ fun HomeScreen(modifier: Modifier = Modifier, navController: NavController, auth
                 color = Pink40
             )
             Spacer(modifier = Modifier.height(16.dp))
-            HeaderSection()
-            Spacer(modifier = Modifier.height(24.dp))
-//            SectionTitle("Dịch vụ")
+            HeaderSection("Chào, Dat Dev", "Chăm sóc bản thân hôm nay nhé")
+            Spacer(modifier = Modifier.height(16.dp))
+            SectionTitle("Dịch vụ")
+            Spacer(modifier = Modifier.height(16.dp))
+            //Services
+            Row (
+                modifier = Modifier.fillMaxWidth()
+            ){
+                ServiceItem("https://placehold.co/50x50/png","Tư vấn")
+                ServiceItem("https://placehold.co/50x50/png","Đặt lịch")
+
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Image(
+                painter = rememberAsyncImagePainter("https://placehold.co/300x160/png"),
+                contentDescription = "08cd217da88c32763e058a99c2f18a72 1",
+                modifier = modifier
+                    .fillMaxWidth()
+                    .requiredHeight(height = 149.dp)
+                    .fillMaxWidth()
+                    .clip(shape = RoundedCornerShape(20.dp)))
+            Spacer(modifier = Modifier.height(16.dp))
+            SaleOffer()
             Spacer(modifier = Modifier.height(16.dp))
             ServiceGrid()
-            Spacer(modifier = Modifier.height(16.dp))
-
         }
     }
 }
 
+
 @Composable
-fun HeaderSection() {
+fun SaleOffer(modifier: Modifier = Modifier) {
+    Surface(
+        shape = RoundedCornerShape(10.dp),
+        color = Color.White,
+        border = BorderStroke(2.dp, Color(0xffa7a7a7)),
+        modifier = modifier
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(51.dp)
+                .padding(horizontal = 16.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.LocalOffer,
+                contentDescription = "Sale Icon",
+                tint = Color.Black,
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Column {
+                Text(
+                    text = "Nhập mã",
+                    color = Color.Black,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Mã ưu đãi, mã giới thiệu",
+                    color = Color.Black.copy(alpha = 0.73f),
+                    fontSize = 14.sp
+                )
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun ServiceItem(symbol: Any?, title: String) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.width(50.dp)
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .background(Color(0xffd0d6da))
+        ) {
+            Image(
+                painter = rememberAsyncImagePainter(symbol),
+                contentDescription = title,
+                modifier = Modifier.size(28.dp)
+            )
+        }
+        Text(
+            text = title,
+            color = Color(0xff1f2022),
+            fontSize = 10.sp,
+            modifier = Modifier.padding(top = 4.dp)
+        )
+    }
+}
+
+@Composable
+fun HeaderSection(title: String, subTitle: String, avatar: Any? = "https://placehold.co/50x50/png") {
     Row(
         modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceBetween,
@@ -86,19 +177,19 @@ fun HeaderSection() {
     ) {
         Column {
             Text(
-                text = "Chào, Jennifer!",
+                text = title,
                 fontSize = 22.sp,
                 fontWeight = FontWeight.Bold,
                 color = Color.Black
             )
             Text(
-                text = "Chăm sóc bản thân hôm nay nhé!",
+                text = subTitle,
                 fontSize = 16.sp,
                 color = Color.Gray
             )
         }
         Image(
-            painter = rememberAsyncImagePainter("https://placehold.co/50x50/png"),
+            painter = rememberAsyncImagePainter(avatar),
             contentDescription = "Hồ sơ",
             modifier = Modifier
                 .size(50.dp)
