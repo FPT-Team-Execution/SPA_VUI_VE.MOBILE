c39813e0669221694bc8c8e94ee50f3733dcdbc1
DatSung <datsung.dev@gmail.com>
2025-02-08 07:33:04 +0700

    chore: move content from home to product

--- app/src/main/java/com/spavv/m/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/spavv/m/ui/screens/home/HomeScreen.kt
@@ -1,23 +1,13 @@
 package com.spavv.m.ui.screens.home
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.spavv.m.comon.constants.Routes
 import com.spavv.m.comon.viewModels.AuthState
 import com.spavv.m.comon.viewModels.AuthVM
-import com.spavv.m.ui.components.general.Header
-import com.spavv.m.ui.components.home.PreviewBannerSlider
 import com.spavv.m.ui.screens.ScaffoldLayout
 
 @Composable
@@ -37,26 +27,7 @@ fun HomeScreen(
     }
 
     ScaffoldLayout(navController) { innerPadding ->
-        Column(
-            modifier = modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-        ) {
-            Header(navController)
-            Spacer(modifier = Modifier.height(16.dp))
-            Column {
-                PreviewBannerSlider()
-                Column {
-                    Text(text = "Danh mục")
-                    Row {
 
-                    }
-                }
-                Column {
-                    Text(text = "Tiêu biểu")
-                }
-            }
-        }
     }
 }
 
--- app/src/main/java/com/spavv/m/ui/screens/product/ProductScreen.kt
+++ app/src/main/java/com/spavv/m/ui/screens/product/ProductScreen.kt
@@ -1,23 +1,41 @@
 package com.spavv.m.ui.screens.product
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.spavv.m.ui.components.general.Header
 import com.spavv.m.ui.screens.ScaffoldLayout
 
 @Composable
 fun ProductScreen(modifier: Modifier, navController: NavController) {
 
-    ScaffoldLayout (navController){
-            innerPadding ->
+    ScaffoldLayout(navController) { innerPadding ->
         Column(
-            modifier = modifier.padding(innerPadding)
+            modifier = modifier
+                .fillMaxSize()
+                .padding(innerPadding)
         ) {
-            Text(text = "Product")
+            Header(navController)
+            Spacer(modifier = Modifier.height(16.dp))
+            Column {
+                Column {
+                    Text(text = "Danh mục")
+                    Row {
 
+                    }
+                }
+                Column {
+                    Text(text = "Tiêu biểu")
+                }
+            }
         }
     }
 
