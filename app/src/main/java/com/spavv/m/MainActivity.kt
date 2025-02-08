0e7a5688a1cabc88d7a51d39e3690198894eb8a5
hukoFpt <lynnie.nguyen.bns@gmail.com>
2025-02-08 11:37:49 +0700

    Adding UI

--- app/src/main/java/com/spavv/m/MainActivity.kt
+++ app/src/main/java/com/spavv/m/MainActivity.kt
@@ -6,79 +6,30 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
+import com.spavv.m.comon.viewModels.AuthVM
+import com.spavv.m.di.MyApp
+import com.spavv.m.helper.viewModelFactory
+import com.spavv.m.ui.screens.login.LoginScreen
+import com.spavv.m.ui.screens.login.LoginVM
 import com.spavv.m.ui.screens.profile.ProfileVM
 import com.spavv.m.ui.theme.SpaVuiVeTheme
-import java.security.KeyManagementException
-import java.security.NoSuchAlgorithmException
-import java.security.SecureRandom
-import java.security.cert.CertificateException
-import java.security.cert.X509Certificate
-import javax.net.ssl.HttpsURLConnection
-import javax.net.ssl.SSLContext
-import javax.net.ssl.TrustManager
-import javax.net.ssl.X509TrustManager
-
-
-val LocalNavigation = staticCompositionLocalOf<NavHostController> { error("Not provided") }
-
+import androidx.navigation.compose.rememberNavController
+import com.spavv.m.ui.screens.profile.ProfileScreen
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        disableSSLCertificateChecking()
         enableEdgeToEdge()
         setContent {
             SpaVuiVeTheme {
-                //create new rememberNavController instance
-                val navController = rememberNavController()
-                CompositionLocalProvider(LocalNavigation provides navController) {
-                    MyAppNavigation(modifier = Modifier
-                        .fillMaxSize()
-                        //safe area
-                        //! DO NOT REMOVE
-                        .statusBarsPadding())
-                }
+                MyAppNavigation(modifier = Modifier.fillMaxSize().padding(16.dp), authVM = viewModel<AuthVM>())
             }
         }
-    }
-}
-
-private fun disableSSLCertificateChecking() {
-    val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
-        override fun getAcceptedIssuers(): Array<X509Certificate> {
-            return emptyArray()
-        }
-
-        @Throws(CertificateException::class)
-        override fun checkClientTrusted(arg0: Array<X509Certificate?>?, arg1: String?) {
-            // Not implemented
-        }
-
-        @Throws(CertificateException::class)
-        override fun checkServerTrusted(arg0: Array<X509Certificate?>?, arg1: String?) {
-            // Not implemented
-        }
-    })
-
-    try {
-        val sc = SSLContext.getInstance("TLS")
-
-        sc.init(null, trustAllCerts, SecureRandom())
-
-        HttpsURLConnection.setDefaultSSLSocketFactory(sc.socketFactory)
-    } catch (e: KeyManagementException) {
-        e.printStackTrace()
-    } catch (e: NoSuchAlgorithmException) {
-        e.printStackTrace()
     }
 }
 
