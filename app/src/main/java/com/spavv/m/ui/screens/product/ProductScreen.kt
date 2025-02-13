56d04c4e80e7af3caa8a922cf9db91c63fa486db
DatSung <datsung.dev@gmail.com>
2025-02-13 11:06:53 +0700

    feat: display product

--- app/src/main/java/com/spavv/m/ui/screens/product/ProductScreen.kt
+++ app/src/main/java/com/spavv/m/ui/screens/product/ProductScreen.kt
@@ -1,24 +1,19 @@
 package com.spavv.m.ui.screens.product
 
-import androidx.compose.foundation.background
+import ProductCard
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -26,7 +21,6 @@ import com.spavv.m.data.dataSources.GetProductsQuery
 import com.spavv.m.di.MyApp
 import com.spavv.m.helper.viewModelFactory
 import com.spavv.m.ui.components.general.Header
-import com.spavv.m.ui.components.product.ProductCard
 import com.spavv.m.ui.screens.ScaffoldLayout
 
 @Composable
@@ -57,16 +51,13 @@ fun ProductScreen(modifier: Modifier, navController: NavController) {
         Column(
             modifier = modifier
                 .fillMaxSize()
-                .padding(16.dp)
+                .padding(
+                    bottom = 100.dp
+                )
         ) {
             Header(navController)
             Spacer(modifier = Modifier.height(16.dp))
             Column {
-                Column {
-                    Text(
-                        text = "Sản phẩm",
-                    )
-
                     when {
                         productVM.isLoading.value -> {
                             CircularProgressIndicator()
@@ -87,9 +78,6 @@ fun ProductScreen(modifier: Modifier, navController: NavController) {
                             }
                         }
                     }
-
-
-                }
             }
         }
     }
