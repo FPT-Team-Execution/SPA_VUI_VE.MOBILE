c77ac42b0d2ebed82a6afe256c559f3b1667eb67
NguyenDat0809 <datngx.dev@gmail.com>
2025-03-11 17:52:25 +0700

    update(viewmodel): implement method handle signin google

--- app/src/main/java/com/spavv/m/comon/viewModels/AuthVM.kt
+++ app/src/main/java/com/spavv/m/comon/viewModels/AuthVM.kt
@@ -1,23 +1,91 @@
 package com.spavv.m.comon.viewModels
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
+import android.provider.Settings.Global.getString
+import android.util.Log
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.spavv.m.R
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 
 open class AuthVM(private val sharedPreferences: SharedPreferences) : ViewModel() {
     private val auth : FirebaseAuth = FirebaseAuth.getInstance();
 
+    lateinit var googleSignInClient : GoogleSignInClient;
+
     private val _authState = MutableLiveData<AuthState>();
     val authState: LiveData<AuthState> = _authState;
 
     init {
         checkAuthState()
+
+    }
+
+    fun createGoogleSignInClient(context: Context){
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("873564041049-df4rtdn53pvua93aql8vl7e9babk0khq.apps.googleusercontent.com") // Lấy ID Token đúng
+            .requestEmail()
+            .build()
+        googleSignInClient =  GoogleSignIn.getClient(context, gso)
+    }
+
+    fun getSignInIntent(): Intent {
+        if (!this::googleSignInClient.isInitialized) {
+            throw IllegalStateException("GoogleSignInClient chưa được khởi tạo. Gọi createGoogleSignInClient(context) trước!")
+        }
+        return googleSignInClient.signInIntent
+    }
+    fun handleSignInResult(task: Task<GoogleSignInAccount>) {
+        viewModelScope.launch {
+            try {
+                val account = task.getResult(ApiException::class.java)
+                val idToken = account?.idToken
+                //Log.d("GoogleSignIn", "ID Token: $idToken")
+
+                if (idToken != null) {
+                    // Gửi token lên server để xác thực Firebase
+                    signInWithGoogle(idToken)
+                }
+            } catch (e: ApiException) {
+               Log.e("GoogleSignIn", "Đăng nhập thất bại: ${e.statusCode}")
+            }
+        }
+    }
+
+    private fun signInWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    //Log.d("GoogleSignIn", "Đăng nhập Firebase thành công!")
+                    _authState.value = AuthState.Authenticated
+                    //save token
+                    viewModelScope.launch {
+                        val token = getToken()
+                        sharedPreferences.edit().apply {
+                            putString("tokenString", token)
+                            apply()
+                        }
+                    }
+                } else {
+                   // Log.e("GoogleSignIn", "Lỗi khi đăng nhập Firebase: ${task.exception?.message}")
+                    _authState.value = AuthState.Error("Đăng nhập thất bại")
+                }
+            }
     }
 
     suspend fun getToken(): String? {
