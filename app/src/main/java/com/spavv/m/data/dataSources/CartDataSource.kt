1d914f1e99b81cf1c1723fc7d62318b8f9c5d982
DatSung <datsung.dev@gmail.com>
2025-03-12 10:16:46 +0700

    feat: implement cart

---
+++ app/src/main/java/com/spavv/m/data/dataSources/CartDataSource.kt
@@ -0,0 +1,53 @@
+package com.spavv.m.data.dataSources
+
+import android.content.SharedPreferences
+import com.spavv.m.data.api.CartApi
+import com.spavv.m.data.models.Item
+import com.spavv.m.data.models.base.BaseResult
+import com.spavv.m.data.models.payload.AddToCartRequest
+import com.spavv.m.exceptions.UnauthorizedException
+
+interface CartDataSource {
+    suspend fun getCart(): List<Item>?
+    suspend fun addToCart(request: AddToCartRequest): BaseResult<String>
+    suspend fun updateFromCart(request: AddToCartRequest): BaseResult<String>
+    suspend fun removeFromCart(id: String): BaseResult<String>
+}
+
+class CartDataSourceImpl(
+    private val cartApi: CartApi,
+    private val sharedPreferences: SharedPreferences
+) : CartDataSource {
+
+    override suspend fun getCart(): List<Item>? {
+        try {
+            val token = sharedPreferences.getString("tokenString", "");
+            if (token.isNullOrEmpty())
+                throw UnauthorizedException("Token is missing")
+            val response = cartApi.getCart(token)
+            if (response.code() == 401) {
+                throw UnauthorizedException("UnAuthorized")
+            }
+            if (response.body()?.status == 200) {
+                return response.body()?.data;
+            }
+            return emptyList()
+        } catch (e: UnauthorizedException) {
+            throw e   //throw exception to ui layer for using LocalNavigation
+        } catch (e: Exception) {
+            return emptyList()
+        }
+    }
+
+    override suspend fun addToCart(request: AddToCartRequest): BaseResult<String> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateFromCart(request: AddToCartRequest): BaseResult<String> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun removeFromCart(id: String): BaseResult<String> {
+        TODO("Not yet implemented")
+    }
+}
