332dfd63122878838ae4e06f2ec9f27eedc9e424
DatSung <datsung.dev@gmail.com>
2025-03-15 08:33:56 +0700

    feat: show cart

--- app/src/main/java/com/spavv/m/ui/screens/cart/CartScreen.kt
+++ app/src/main/java/com/spavv/m/ui/screens/cart/CartScreen.kt
@@ -5,22 +5,41 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.spavv.m.LocalNavigation
+import com.spavv.m.di.MyApp
+import com.spavv.m.helper.viewModelFactory
 import com.spavv.m.ui.screens.ScaffoldLayout
 
 //import com.spavv.m.ui.screens.ScaffoldLayout
 
 @Composable
 fun CartScreen(modifier: Modifier) {
+
     val navController = LocalNavigation.current;
-    ScaffoldLayout (navController){
-            innerPadding ->
+    val cartVM = viewModel<CartVM>(
+        factory = viewModelFactory {
+            CartVM(
+                MyApp.appModule.cartDataSource
+            )
+        }
+    )
+
+    LaunchedEffect(Unit) {
+        cartVM.fetchCart()
+    }
+
+    ScaffoldLayout(navController) { innerPadding ->
         Column(
             modifier = modifier.padding(innerPadding)
         ) {
-            Text(text = "Cart")
+            cartVM.cart.value?.forEach { it ->
+                Text(text = it.product.name)
+            }
+
         }
     }
 
--- app/src/main/java/com/spavv/m/ui/screens/cart/CartVM.kt
+++ app/src/main/java/com/spavv/m/ui/screens/cart/CartVM.kt
@@ -1,2 +1,38 @@
 package com.spavv.m.ui.screens.cart
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.spavv.m.data.dataSources.CartDataSource
+import com.spavv.m.data.models.Item
+import androidx.compose.runtime.State
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class CartVM(
+    private val cartDataSource: CartDataSource
+) : ViewModel() {
+    var isLoading = mutableStateOf<Boolean>(false);
+
+    private val _cart = mutableStateOf<List<Item>?>(null)
+    val cart: State<List<Item>?> = _cart;
+
+    private fun updateCart(cart: List<Item>?) {
+        _cart.value = cart
+    }
+
+    fun fetchCart() {
+        isLoading.value = true;
+        viewModelScope.launch {
+            try {
+                val cart = cartDataSource.getCart();
+                updateCart(cart)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+
+
+
+}
